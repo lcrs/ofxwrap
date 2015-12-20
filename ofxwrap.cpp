7393ea299d7943db4790340eddbea56ea883ace7
Lewis Saunders <lewis.saunders@gmail.com>
2015-12-20 14:38:02 +0000

    calling load action, plugin calls back for suites (sweets?)

--- ofxwrap.cpp
+++ ofxwrap.cpp
@@ -3,6 +3,14 @@
 #include "/usr/discreet/presets/2016/sparks/spark.h"
 #include "openfx/include/ofxCore.h"
 
+OfxPlugin *plugin = NULL;
+OfxPropertySetHandle hostpropset = NULL;
+
+const void *fetchSuite(OfxPropertySetHandle host, const char *suite, int version) {
+	printf("Ofxwrap: fetchSuite() asked for suite %s version %d\n", suite, version);
+	return NULL;
+}
+
 int bufferReady(int id, SparkMemBufStruct *b) {
 	if(!sparkMemGetBuffer(id, b)) {
 		printf("Ofxwrap: Failed to get buffer %d\n", id);
@@ -20,14 +28,12 @@ unsigned int SparkInitialise(SparkInfoStruct si) {
 
 	void *dlhandle = dlopen("/Library/OFX/Plugins/NeatVideo4.ofx.bundle/Contents/MacOS/NeatVideo4.ofx", RTLD_LAZY);
 	if(dlhandle == NULL) {
-		printf("Ofxwrap: dlopen() failed\n");
 		sparkError("Ofxwrap: failed to dlopen() OFX plugin!");
 	}
 	
 	int (*OfxGetNumberOfPlugins)(void);
 	OfxGetNumberOfPlugins = (int(*)(void)) dlsym(dlhandle, "OfxGetNumberOfPlugins");
 	if(OfxGetNumberOfPlugins == NULL) {
-		printf("Ofxwrap: dlsym() failed\n");
 		sparkError("Ofxwrap: failed to find plugin's OfxGetNumberOfPlugins symbol!\n");
 	}
 	int numplugs = (*OfxGetNumberOfPlugins)();
@@ -36,16 +42,35 @@ unsigned int SparkInitialise(SparkInfoStruct si) {
 	OfxPlugin *(*OfxGetPlugin)(int nth);
 	OfxGetPlugin = (OfxPlugin*(*)(int nth)) dlsym(dlhandle, "OfxGetPlugin");
 	if(OfxGetPlugin == NULL) {
-		printf("Ofxwrap: dlsym() failed\n");
 		sparkError("Ofxwrap: failed to find plugin's OfxGetPlugin symbol!\n");
 	}
-	OfxPlugin *p = (*OfxGetPlugin)(0);
-	if(p == NULL) {
-		printf("Ofxwrap: OfxGetPlugin() failed\n");
-		sparkError("Ofxwrap: failed to get plugin 1!\n");
+	plugin = (*OfxGetPlugin)(0);
+	if(plugin == NULL) {
+		sparkError("Ofxwrap: failed to get first plugin!\n");
 	}
+	printf("Ofxwrap: plugin id is %s\n", plugin->pluginIdentifier);
 
-	printf("Ofxwrap: plugin id is %s\n", p->pluginIdentifier);
+	OfxHost h;
+	h.host = hostpropset;
+	h.fetchSuite = &fetchSuite;
+	plugin->setHost(&h);
+	OfxStatus s = plugin->mainEntry(kOfxActionLoad, NULL, NULL, NULL);
+	switch(s) {
+		case kOfxStatOK:
+			printf("Ofxwrap: load action ok\n");
+			break;
+		case kOfxStatReplyDefault:
+			printf("Ofxwrap: load action ignored!\n");
+			break;
+		case kOfxStatFailed:
+			sparkError("Ofxwrap: load action failed!");
+		case kOfxStatErrFatal:
+			sparkError("Ofxwrap: load action fatal error!");
+		case kOfxStatErrMissingHostFeature:
+			sparkError("Ofxwrap: load action missing feature!");
+		default:
+			printf("Ofxwrap: load action returned %d\n", s);
+	}
 
 	return(SPARK_MODULE);
 }
