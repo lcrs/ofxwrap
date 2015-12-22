c0a2ce3d5c5c1d39231b64a096add027a2a52932
Lewis Saunders <lewis.saunders@gmail.com>
2015-12-22 02:33:32 +0000

    briefly converted to standalone executable, create action now works :(

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: ofxwrap.spark
 
 ofxwrap.spark: ofxwrap.o Makefile
-	g++ -arch x86_64 -dynamiclib -undefined dynamic_lookup ofxwrap.o -o ofxwrap.spark
+	g++ -arch x86_64 ofxwrap.o -o ofxwrap.spark
 
 ofxwrap.o: ofxwrap.cpp props.h dialogs.h ifxs.h parms.h half.h halfExport.h
 	g++ -O0 -g -fPIC -DDL_LITTLE_ENDIAN -D_DARWIN_USE_64_BIT_INODE -arch x86_64 -c ofxwrap.cpp -o ofxwrap.o
--- ofxwrap.cpp
+++ ofxwrap.cpp
@@ -1,11 +1,15 @@
 #include "half.h"
 #include <dlfcn.h>
-#include "/usr/discreet/presets/2016/sparks/spark.h"
+
 #include "openfx/include/ofxCore.h"
 #include "props.h"
 #include "dialogs.h"
 #include "ifxs.h"
 #include "parms.h"
+
+void sparkError(const char *s) {
+	printf("%s", s);
+}
 
 const void *fetchSuite(OfxPropertySetHandle host, const char *suite, int version) {
 	printf("Ofxwrap: fetchSuite() asked for suite %s version %d\n", suite, version);
@@ -28,21 +32,7 @@ const void *fetchSuite(OfxPropertySetHandle host, const char *suite, int version
 	return NULL;
 }
 
-int bufferReady(int id, SparkMemBufStruct *b) {
-	if(!sparkMemGetBuffer(id, b)) {
-		printf("Ofxwrap: Failed to get buffer %d\n", id);
-		return 0;
-	}
-	if(!(b->BufState & MEMBUF_LOCKED)) {
-		printf("Ofxwrap: Failed to lock buffer %d\n", id);
-		return 0;
-	}
-	return 1;
-}
-
-unsigned int SparkInitialise(SparkInfoStruct si) {
-	printf("\n\n\nOfxwrap: in SparkInitialise(), name is %s\n", si.Name);
-
+int main(int argc, char **argv){
 	void *dlhandle = dlopen("/Library/OFX/Plugins/NeatVideo4.ofx.bundle/Contents/MacOS/NeatVideo4.ofx", RTLD_LAZY);
 	if(dlhandle == NULL) {
 		sparkError("Ofxwrap: failed to dlopen() OFX plugin!");
@@ -134,44 +124,5 @@ unsigned int SparkInitialise(SparkInfoStruct si) {
 			printf("Ofxwrap: create action: returned %d\n", s);
 	}
 
-	return(SPARK_MODULE);
-}
-
-int SparkClips(void) {
 	return 1;
-}
-
-unsigned long *SparkProcess(SparkInfoStruct si) {
-	printf("Ofxwrap: in SparkProcess(), name is %s\n", si.Name);
-
-	SparkMemBufStruct result, front;
-	if(!bufferReady(1, &result)) return(NULL);
-	if(!bufferReady(2, &front)) return(NULL);
-
-	// kOfxImageEffectActionRender
-
-	return NULL;
-}
-
-void SparkUnInitialise(SparkInfoStruct si) {
-	printf("Ofxwrap: in SparkUnInitialise(), name is %s\n", si.Name);
-}
-
-void SparkMemoryTempBuffers(void) {
-}
-
-int SparkIsInputFormatSupported(SparkPixelFormat fmt) {
-	if(fmt == SPARKBUF_RGB_48_3x16_FP) {
-		return 1;
-	} else {
-		return 0;
-	}
-}
-
-void SparkEvent(SparkModuleEvent e) {
-	printf("Ofxwrap: in SparkEvent(), event is %d, last setup name is %s\n", (int)e, sparkGetLastSetupName());
-}
-
-void SparkSetupIOEvent(SparkModuleEvent e, char *path, char *file) {
-	printf("Ofxwrap: in SparkIOEvent(), event is %d, path is %s, file is %s\n", (int)e, path, file);
 }
